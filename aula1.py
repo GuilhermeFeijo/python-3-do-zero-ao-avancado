7b85e812b8968007cbe0dd7e364e7f381788d2ef
GuilhermeFeijo <guilhermezfeijo99@gmail.com>
2026-05-05 20:18:39 -0300

    finalização da aula1.py

--- aula1.py
+++ aula1.py
@@ -1,1 +1,14 @@
-print('Atenção')
+"""
+DocString
+E escrever o que eu quiser
+"""
+
+'''
+Usar para escrever suas notas
+ou para explicar o código
+'''
+
+# Permite escrever um comentário
+print(123) # Na frente
+# Abaixo
+print(456)
