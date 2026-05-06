37a7a704ec34b66ae46f994b68949713f5c3b5bd
GuilhermeFeijo <guilhermezfeijo99@gmail.com>
2026-05-06 20:46:12 -0300

    feat: finalizado aula 4

---
+++ aula4.py
@@ -0,0 +1,18 @@
+### Tipos int e float
+
+# int: números inteiros
+# O tipo int representa qualquer número positivo ou negativo. int sem sinal é considerado positivo
+# print(11) #int
+# print(-11) #int
+# print(0) #int
+
+# float: números decimais(ponto flutuante)
+# O tipo float representa números decimais, ou seja, números com parte fracionária. Ele é usado para representar valores que não são inteiros, como 3.14, -0.5, ou 2.0.
+print(1.1, 10.11) #float
+print(-1.1) #float
+print(0.0) #float
+
+# A função type() pode ser usada para verificar o tipo de um valor ou variável
+print(type(11)) #int
+print(type(1.1)) #float
+print(type("Batata")) #str
