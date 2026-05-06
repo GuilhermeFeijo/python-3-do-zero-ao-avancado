88b1e7b34e0314fba7a3edcba1ad5c7b9c6ccc5c
GuilhermeFeijo <guilhermezfeijo99@gmail.com>
2026-05-06 20:28:23 -0300

    feat: aula3 ok

---
+++ aula3.py
@@ -0,0 +1,20 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos dentro de aspas
+"""
+
+# Aspas simples
+print('Isso é uma string')
+print('Isso é uma "string"')
+
+# Aspas duplas
+print("Isso é uma string")
+print("Isso é uma 'string'")
+
+# Escape
+print('Isso é uma string com aspas simples \'dentro\' dela')
+
+# r
+print(r'Isso é uma string com aspas simples \'dentro\' dela')
