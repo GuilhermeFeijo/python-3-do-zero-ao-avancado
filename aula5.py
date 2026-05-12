06ab9fef470fe34060c319b66be58628e67add7b
GuilhermeFeijo <guilhermezfeijo99@gmail.com>
2026-05-12 20:36:56 -0300

    feat: aula5 ok

---
+++ aula5.py
@@ -0,0 +1,10 @@
+# Tipo de dado bool (booleano)
+# Ao questionar algo em um programa, só existem duas respostas possíveis: Sim (True) ou Não (False).
+# Existem vários operadores para "questionar" algo, como: == (igual a), != (diferente de)...
+
+print(10 == 10) # True
+print(10 == 11) # False
+print(type(True))
+print(type(False))
+print(type(10 == 10)) # O resultado de uma comparação é do tipo bool
+print(type(10 == 11)) # O resultado de uma comparação é do tipo bool
